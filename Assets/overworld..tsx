f6977d468f124d0d7aac412da84f857bdcca657c
loporean <84533280+loporean@users.noreply.github.com>
2022-09-29 17:44:13 -0700

    File Uploads

---
+++ Assets/overworld..tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="Art/gfx/Overworld.png" width="640" height="576"/>
+ <wangsets>
+  <wangset name="path" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1160" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1161" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="1162" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="1200" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="1201" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="1202" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="1240" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="1241" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="1242" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="1280" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="1281" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="1320" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="1321" wangid="0,1,0,1,0,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # PortalSword
-ARPG zelda like game based off of zelda and Death's Door
